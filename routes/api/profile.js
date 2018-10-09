d6980ed0ae24aeac4317da3f99dca8cd1ad81f9e
Igor Savin <beixiaofeng@gmail.com>
2018-10-09 14:18:50 +0300

    get and delete posts route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -270,9 +270,9 @@ router.post(
   // @access Private
 
   router.delete('/', passport.authenticate('jwt',{session: false}),(req,res)=>{
-   Profile.findOneAndRemove({user:req.user.id})
+   Profile.findOneAndDelete({user:req.user.id})
    .then(()=>{
-     User.findOneAndRemove({_id:req.user.id}).then(()=> res.json({success:true})
+     User.findOneAndDelete({_id:req.user.id}).then(()=> res.json({success:true})
      )
    })
   })
