be8fef543b9fd061d8e27429354557844b557e11
Igor Savin <beixiaofeng@gmail.com>
2018-10-15 23:15:50 +0300

    added profile reducer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import {setCurrentUser, logoutUser} from './actions/authActions';
+import {clearCurrentProfile} from './actions/profileActions';
 
 
 import {Provider} from 'react-redux';
@@ -13,6 +14,7 @@ import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -30,7 +32,8 @@ const currentTime = Date.now()/1000;
 if(decoded.exp < currentTime){
   //Logout User
   store.dispatch(logoutUser());
-  //TODO:Clear current Profile
+  //Clear current Profile
+  store.dispatch(clearCurrentProfile());
   //Redirect to login
   window.location.href= '/login';
 }
@@ -48,6 +51,7 @@ class App extends Component {
         <div className="container">
         <Route exact path="/register" component={Register}/>
         <Route exact path="/login" component={Login}/>
+        <Route exact path="/dashboard" component={Dashboard}/>
         </div>
         <Footer/> 
       </div>
