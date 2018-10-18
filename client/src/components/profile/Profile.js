5f9f2a31ee821cc7bca9d88eda24ac9726eef5ee
Igor Savin <beixiaofeng@gmail.com>
2018-10-18 23:55:50 +0300

    changed profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -13,25 +13,57 @@ import {getProfileByHandle} from '../../actions/profileActions'
 class Profile extends Component {
   componentDidMount(){
     if(this.props.match.params.handle){
-      this.props.getProfileByHandle(this.props.match.params.handle)
+      this.props.getProfileByHandle(this.props.match.params.handle);
     } 
   }
 
   render() {
+    const {profile, loading} = this.props.profile;
+    let profileContent;
+
+    if(profile===null|| loading){
+      profileContent =<Spinner/>
+    }else{
+      profileContent =(
+        <div>
+          <div className="row">
+          <div className="col-md-6">
+          <Link to="/profiles" className="btn btn-light mb-3 float-left">
+            Back To Ptofiles
+          </Link>
+          </div>
+          <div className="col-md-6"></div>
+          </div>
+          <ProfileHeader profile={profile}/>
+        <ProfileAbout />
+        <ProfileCreds />
+        <ProfileGithub />
+        </div>
+      )
+    }
+
     return (
-      <div>
-        <h1>TODO: Profile </h1>
+      <div className="profile">
+      <div className="comtainer">
+        <div className="row">
+          <div className="col-md-12">
+          {profileContent}
+          </div>
+        </div>
+      </div>
+        
       </div>
     )
   }
 }
 
 Profile.propTypes ={
-  profile:PropTypes.object.isRequired
+  profile:PropTypes.object.isRequired,
+  getProfileByHandle: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state =>({
   profile:state.profile
 })
 
-export default connect(mapStateToProps)(Profile);
+export default connect(mapStateToProps, {getProfileByHandle})(Profile);
